0bf3d0fd422e97dea4ca07c6c2233f96de15349e
dibsk09 <sn2025045@shs.hs.kr>
2025-06-10 14:39:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import streamlit as st
 st.title("첫 app")
 st.write("hello")
+name=st.text_input("이름 입력")
+if name:
+  if name=="연우":
+    st.success("반갑습니다 ,name,님")
+  else:
+    st.warning("누구니????")
+
+  
